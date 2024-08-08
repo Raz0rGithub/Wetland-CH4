ec3f120e9e1a992c2b45634121ed94f08bbd3aae
Victor Chen <vcraz0r@gmail.com>
2024-08-08 01:50:54 -0700

    work with dataset flux charts (making them interactive)

--- script.js
+++ script.js
@@ -7,6 +7,69 @@ $(document).ready(function () {
 $(document).ready(function () {
   $("#truefooter").load("./footer.html");
 });
+/* Flux maps */
+$(document).ready(function () {
+  $("#flux-global").load("./charts/flux-yearly/yearly.html");
+});
+$(document).ready(function () {
+  $("#flux-global-1").load("./charts/flux-yearly/yearly1440.html");
+});
+$(document).ready(function () {
+  $("#flux-global-2").load("./charts/flux-yearly/yearly1080.html");
+});
+$(document).ready(function () {
+  $("#flux-global-3").load("./charts/flux-yearly/yearly720.html");
+});
+$(document).ready(function () {
+  $("#flux-global-4").load("./charts/flux-yearly/yearly480.html");
+});
+$(document).ready(function () {
+  $("#flux-global-5").load("./charts/flux-yearly/yearly360.html");
+});
+
+
+function resize(x) {
+  if (x.matches) { // If media query matches
+    $("#flux-global").load("./charts/flux-yearly/yearly.html");
+  }
+  else if (x1.matches) {
+    $("#flux-global").load("./charts/flux-yearly/yearly1440.html");
+  }
+  else if (x2.matches) {
+    $("#flux-global").load("./charts/flux-yearly/yearly1080.html");
+  }
+  else if (x3.matches) {
+    $("#flux-global").load("./charts/flux-yearly/yearly720.html");
+  }
+  else if (x4.matches) {
+    $("#flux-global").load("./charts/flux-yearly/yearly480.html");
+  }
+  else if (x5.matches) {
+    $("#flux-global").load("./charts/flux-yearly/yearly360.html");
+  }
+}
+// Create a MediaQueryList object
+var x = window.matchMedia("(max-width: 1440px)")
+var x1 = window.matchMedia("(max-width: 1440px)")
+var x2 = window.matchMedia("(max-width: 1080px)")
+var x3 = window.matchMedia("(max-width: 720px)")
+var x4 = window.matchMedia("(max-width: 480px)")
+var x5 = window.matchMedia("(max-width: 360px)")
+resize(x);
+// Attach listener function on state changes
+x.addEventListener("change", function () {
+  resize(x);
+});
+x1.addEventListener("change", function () {
+  resize(x1);
+});
+x2.addEventListener("change", function () {
+  resize(x2);
+});
+x3.addEventListener("change", function () {
+  resize(x3);
+});
+
 
 /** about.html **/
 /* dropdown menu*/
